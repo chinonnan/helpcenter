7985f775d29a51234dfe5f5a8e2479f2f1f095f0
chenennan <chenennan@genomics.cn>
2023-05-16 14:35:26 +0800

    侧边栏功能优化

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,5 +1,15 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
-  "/zh/": "structure",
+  "/zh/": [
+    "",
+    {
+      icon: "creative",
+      text: "版本更新",
+      prefix: "update/",
+      link: "update/",
+      children: "structure",
+      collapsible: true,
+    },
+  ],
 });
